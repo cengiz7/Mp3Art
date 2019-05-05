3b9cc5e46fe626f31fe19d9815b92501afcd62d9
Cengiz Ucgul <cengiz.ucgul@bulutfon.com>
2019-05-05 13:56:44 +0300

    file name fixing and saving finished

--- jobs/FileNameFixer.go
+++ jobs/FileNameFixer.go
@@ -1,11 +1,12 @@
 package jobs
 
 import (
+	"fmt"
 	"log"
 	"os"
-	"fmt"
 	"path/filepath"
 	"strings"
+	"unicode"
 
 	"golang.org/x/exp/errors"
 )
@@ -39,10 +40,17 @@ func getFileNames (folderPath string ) ( []string, string, error ){
 	return files,root,nil
 }
 
-func fixAndChangeFileNames(files *[]string) {
-	for _, filePath := range *files {
+func fixAndSaveFileName(files *[]string,save bool) {
+	for i, filePath := range *files {
 		if strings.HasSuffix(filePath,".mp3") {
-			fmt.Println(filePath)
+			(*files)[i] = exposeMusicName(filePath,(*files)[0])
+			if save {
+				err := os.Rename(filePath, (*files)[0]+ "/" + (*files)[i])
+				if err != nil {
+					log.Println("File rename failed:\n", filePath+ "\to: "+ (*files)[i])
+				}
+			}
+			fmt.Println((*files)[i])
 		} else {
 			// do not puts ignored musics root folder
 			if  !strings.HasSuffix(filePath,"/musics") {
@@ -52,16 +60,63 @@ func fixAndChangeFileNames(files *[]string) {
 	}
 }
 
+func clearIfSepWithLine ( name *string ) {
+	// if ( - ) count more than 1, words can be seperated with -
+	// so replace them with white spaces
+	if strings.Count( *name, "-") > 1 {
+		*name = strings.Replace(*name, "-", " ", -1)
+	}
+}
+
+func clearBrackets( name string ) string {
+	var fixedName string
+	nameLength := len(name)
+	for i := 0; i < nameLength; i++ {
+		if name[i] == '[' || name[i] == '{' {
+			expect := name[i] + 2 // checked from ascii table
+			k := i
+			for ; k < nameLength && name[k] != expect ; k++ {}
+			if name[k] == expect {
+				i = k
+			}
+		} else {
+			fixedName += string(name[i])
+		}
+	}
+	return fixedName
+}
+
+func trimRootPath( name,root *string ) {
+	*name = strings.Replace(*name,*root+"/","",1)
+}
+
+func exposeMusicName (name, root string) string {
+	var fixedName string
+	trimRootPath(&name, &root)
+	clearIfSepWithLine(&name)
+	name = clearBrackets(name)
+	for _, r := range name {
+		if !unicode.IsLetter(r) {
+			switch r {
+			case '_', '^' , '+' , ';' , ':' , '"', ',' :
+				fixedName += " "
+			default:
+				fixedName += string(r)
+			}
+		} else {
+			fixedName += string(r)
+		}
+	}
+	return fixedName
+}
+
 func FixFileNames (folderPath string,save bool) (string, error ){
 
 	files , path , err := getFileNames(folderPath); if err != nil {
 		log.Println("Couldn't get music file names: ",err)
 		return path, err
 	}
-	if save == true {
-		fixAndChangeFileNames(&files)
-	}
-
+	fixAndSaveFileName(&files, save)
 	return path, nil
 }
 
